d1869a85012fb569d6fc4f873cba2bf6d9023a29
陶辰 <chen.tao@ifchange.com>
2020-04-13 20:06:11 +0800

    update

---
+++ minio_cluster_deploy/minio_cluster_deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pkill minio
+export MINIO_ACCESS_KEY=admin
+export MINIO_SECRET_KEY=admin123
+cd /data/nfs
+mkdir -p /opt/data/minio/
+nohup ./minio server http://192.168.3.192/export2 http://192.168.3.193/export3 http://192.168.3.194/export4 http://192.168.3.195/export5 > /opt/data/minio/minio.log 2>&1 &
